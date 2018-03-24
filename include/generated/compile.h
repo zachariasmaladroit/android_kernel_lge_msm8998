4c44ab87dfe12f859fe81767cb260081fc573e12
SGCMarkus <markusornik@gmail.com>
2018-03-24 13:55:42 +0100

    import LGE H930 Oreo sources

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Mar 24 13:46:26 CET 2018"
+#define LINUX_COMPILE_BY "markus"
+#define LINUX_COMPILE_HOST "markus-pc"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
