cb9897010738c66196756904253288cf36b027fe
Demon Singur <demonsingur@gmail.com>
2018-04-05 11:29:37 +0000

    qcacld-3.0: guard unused function for in-kernel builds
    Change-Id: Ia9c7e27519faa85b07089a7ee26623c00c25e0d3
    
    wlan_hdd_state_ctrl_param_destroy();
    
    call in 'static void __hdd_module_exit(void)' needs to be removed as
    well:
    
    "Module exit helper function used by both module and static driver."
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -11297,6 +11297,7 @@ dev_alloc_err:
 	return -ENODEV;
 }
 
+#ifdef MODULE
 static void wlan_hdd_state_ctrl_param_destroy(void)
 {
 	cdev_del(&wlan_hdd_state_cdev);
@@ -11306,6 +11307,7 @@ static void wlan_hdd_state_ctrl_param_destroy(void)
 
 	pr_info("Device node unregistered");
 }
+#endif
 
 /**
  * __hdd_module_init - Module init helper
@@ -11381,7 +11383,9 @@ static void __hdd_module_exit(void)
 
 	hdd_deinit();
 	pld_deinit();
+#ifdef MODULE
 	wlan_hdd_state_ctrl_param_destroy();
+#endif
 }
 
 #ifndef MODULE
