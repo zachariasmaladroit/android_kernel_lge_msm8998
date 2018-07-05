11cdbc7ca17049958555da466c3fb4ffca4b926b
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-07-05 04:17:46 +0200

    Revert "sched / idle: Drop default_idle_call() fallback from call_cpuidle()"
    This reverts commit a3911670c4662ac340352137c0bcb0de30c1e8ec.

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -79,9 +79,9 @@ static int find_deepest_state(struct cpuidle_driver *drv,
 			      bool freeze)
 {
 	unsigned int latency_req = 0;
-	int i, ret = 0;
+	int i, ret = -ENXIO;
 
-	for (i = 1; i < drv->state_count; i++) {
+	for (i = 0; i < drv->state_count; i++) {
 		struct cpuidle_state *s = &drv->states[i];
 		struct cpuidle_state_usage *su = &dev->states_usage[i];
 
@@ -247,7 +247,7 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
  * @drv: the cpuidle driver
  * @dev: the cpuidle device
  *
- * Returns the index of the idle state.  The return value must not be negative.
+ * Returns the index of the idle state.
  */
 int cpuidle_select(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 {
