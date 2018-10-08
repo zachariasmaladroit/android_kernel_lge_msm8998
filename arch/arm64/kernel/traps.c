23edfa27ffa2cb6ba2bc267698c9fecb6e04fe4c
Mark Rutland <mark.rutland@arm.com>
2018-10-08 02:26:50 +0200

    [eval][PORT|joan] UPSTREAM: arm64: traps: simplify die() and __die()
    In arm64's die and __die routines we pass around a thread_info, and
    subsequently use this to determine the relevant task_struct, and the end
    of the thread's stack. Subsequent patches will decouple thread_info from
    the stack, and this approach will no longer work.
    
    To figure out the end of the stack, we can use the new generic
    end_of_stack() helper. As we only call __die() from die(), and die()
    always deals with the current task, we can remove the parameter and have
    both acquire current directly, which also makes it clear that __die
    can't be called for arbitrary tasks.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Tested-by: Laura Abbott <labbott@redhat.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 38331309
    Change-Id: Ie1a96a0a8e244d458a7f147001b64216403e07c4
    (cherry picked from commit 876e7a38e8788773aac768091aaa3b42e470c03b)
    Signed-off-by: Zubin Mithra <zsm@google.com>
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -254,10 +254,9 @@ void show_stack(struct task_struct *tsk, unsigned long *sp)
 extern DEFINE_PER_CPU(struct pt_regs, regs_before_stop);
 #endif
 
-static int __die(const char *str, int err, struct thread_info *thread,
-		 struct pt_regs *regs)
+static int __die(const char *str, int err, struct pt_regs *regs)
 {
-	struct task_struct *tsk = thread->task;
+	struct task_struct *tsk = current;
 	static int die_counter;
 	int ret;
 
@@ -275,7 +274,8 @@ static int __die(const char *str, int err, struct thread_info *thread,
 #endif
 	__show_regs(regs);
 	pr_emerg("Process %.*s (pid: %d, stack limit = 0x%pP)\n",
-		 TASK_COMM_LEN, tsk->comm, task_pid_nr(tsk), thread + 1);
+		 TASK_COMM_LEN, tsk->comm, task_pid_nr(tsk),
+		 end_of_stack(tsk));
 
 	if (!user_mode(regs) || in_interrupt()) {
 		dump_backtrace(regs, tsk);
@@ -340,7 +340,6 @@ static void oops_end(unsigned long flags, struct pt_regs *regs, int notify)
  */
 void die(const char *str, struct pt_regs *regs, int err)
 {
-	struct thread_info *thread = current_thread_info();
 	enum bug_trap_type bug_type = BUG_TRAP_TYPE_NONE;
 	unsigned long flags = oops_begin();
 	int ret;
@@ -350,7 +349,7 @@ void die(const char *str, struct pt_regs *regs, int err)
 	if (bug_type != BUG_TRAP_TYPE_NONE)
 		str = "Oops - BUG";
 
-	ret = __die(str, err, thread, regs);
+	ret = __die(str, err, regs);
 
 	oops_end(flags, regs, ret);
 }
