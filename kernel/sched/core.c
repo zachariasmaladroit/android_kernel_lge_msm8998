c2c983bb657445b1f7cc30e15fcd911d7f6c3b1c
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-10-12 18:20:49 +0200

    Revert "sched/core: Correct off by one bug in load migration calculation"
    This reverts commit 05c6511f8a377b32f53ec855b814bea5bf80233b.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5805,15 +5805,13 @@ void idle_task_exit(void)
 /*
  * Since this CPU is going 'away' for a while, fold any nr_active delta
  * we might have. Assumes we're called after migrate_tasks() so that the
- * nr_active count is stable. We need to take the teardown thread which
- * is calling this into account, so we hand in adjust = 1 to the load
- * calculation.
+ * nr_active count is stable.
  *
  * Also see the comment "Global load-average calculations".
  */
 static void calc_load_migrate(struct rq *rq)
 {
-	long delta = calc_load_fold_active(rq, 1);
+	long delta = calc_load_fold_active(rq);
 	if (delta)
 		atomic_long_add(delta, &calc_load_tasks);
 }
