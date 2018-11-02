71593015974ec0a6b965447288c7f22862f84a9d
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-11-02 21:15:17 +0100

    Revert "stop_machine: Don't disable preemption in stop_two_cpus()"
    This reverts commit a00051d1ba080df9af7612a3c573597baf567b98.

--- kernel/stop_machine.c
+++ kernel/stop_machine.c
@@ -283,6 +283,7 @@ int stop_two_cpus(unsigned int cpu1, unsigned int cpu2, cpu_stop_fn_t fn, void *
 	struct cpu_stop_work work1, work2;
 	struct multi_stop_data msdata;
 
+	preempt_disable();
 	msdata = (struct multi_stop_data){
 		.fn = fn,
 		.data = arg,
@@ -301,12 +302,16 @@ int stop_two_cpus(unsigned int cpu1, unsigned int cpu2, cpu_stop_fn_t fn, void *
 
 	if (cpu1 > cpu2)
 		swap(cpu1, cpu2);
-	if (cpu_stop_queue_two_works(cpu1, &work1, cpu2, &work2))
+	if (cpu_stop_queue_two_works(cpu1, &work1, cpu2, &work2)) {
+		preempt_enable();
 		return -ENOENT;
+	}
+
+	preempt_enable();
 
 	wait_for_completion(&done.completion);
-	WARN_ON(!done.executed);
-	return done.ret;
+
+	return done.executed ? done.ret : -ENOENT;
 }
 
 /**
