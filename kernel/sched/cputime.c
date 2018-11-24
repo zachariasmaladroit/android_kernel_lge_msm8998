e1ecf31a8713eb94bf95e1e0b9ace71cddf0ebe9
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-11-25 00:50:35 +0100

    [EVAL|derp] sched/cputime [2]: Drop local_irq_save/restore from irqtime_account_irq()
    attempt to fix this mess for HMP/non-WALT
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -80,8 +80,6 @@ void irqtime_account_irq(struct task_struct *curr)
 		sched_account_irqtime(cpu, curr, delta, wallclock);
 	else if (curr != this_cpu_ksoftirqd())
 		sched_account_irqstart(cpu, curr, wallclock);
-
-	local_irq_restore(flags);
 }
 EXPORT_SYMBOL_GPL(irqtime_account_irq);
 
